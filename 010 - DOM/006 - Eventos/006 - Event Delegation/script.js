234b81f81d2344e712064c1b6f9d445f3eab93ce
Dener Troquatte <you@example.com>
2022-09-01 14:32:52 -0300

    Aulas de Javascript

--- 010 - DOM/006 - Eventos/006 - Event Delegation/script.js
+++ 010 - DOM/006 - Eventos/006 - Event Delegation/script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const menu = document.querySelector("#menu");
+
+menu.addEventListener("click", (event) => {
+  const { target } = event;
+  const body = document.querySelector("body");
+
+  switch (target.getAttribute("class")) {
+    case "home":
+      body.style.background = "blue";
+      break;
+
+    case "sobre":
+      body.style.background = "red";
+      break;
+
+    case "contato":
+      body.style.background = "#fff";
+      break;
+  }
+});
---
+++ 010 - DOM/006 - Eventos/007 - Validando eventos/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const menu = document.querySelector("#menu");
+
+if (menu) {
+  menu.addEventListener("click", (event) => {
+    const { target } = event;
+    const body = document.querySelector("body");
+
+    switch (target.getAttribute("class")) {
+      case "home":
+        body.style.background = "blue";
+        break;
+
+      case "sobre":
+        body.style.background = "red";
+        break;
+
+      case "contato":
+        body.style.background = "#fff";
+        break;
+    }
+  });
+}
